c1f672df83d83ae0ae736765a905bb0ad40b828f
rajob <rajob@DIONE>
2018-04-04 09:19:27 +0530

    non trivial change

--- src/ui/UINotCreatedException.java
+++ src/ui/UINotCreatedException.java
@@ -1,7 +1,7 @@
 package ui;
 
-public class UINotCreatedException extends RuntimeException {
 
+public class UINotCreatedException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 }
